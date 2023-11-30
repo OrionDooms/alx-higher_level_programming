c0af524e62d139c4f0f40f6eaaa0cfc924884922
OrionDooms <orion45.dd@gmail.com>
2023-11-30 08:56:00 -0800

    prints the number of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,11 @@ if __name__ == "__main__":
     if c == 0:
         print("{} arguments.".format(c))
     else:
-        print("{} arguments:".format(c))
-        for i in range(c):
-           count = i + 1
-           print("{}: {}".format(count, sys.argv[count]))
+        if c == 1:
+            print("1 argument:")
+            print("{}: {}".format(c, sys.argv[c]))
+        else:
+            print("{} arguments:".format(c))
+            for i in range(c):
+                count = i + 1
+                print("{}: {}".format(count, sys.argv[count]))
