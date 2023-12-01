67440406b3b802627d77782d6e706d13c14cefa7
OrionDooms <orion45.dd@gmail.com>
2023-12-01 06:44:17 -0800

    access to an element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx >= 0:
+        for i in range(len(my_list)):
+            if idx < i:
+                return ('none')
+            elif idx == i:
+                return (my_list[i])
+    else:
+        return ('none')
