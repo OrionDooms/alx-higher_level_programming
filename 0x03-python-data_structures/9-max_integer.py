daf9fa510021f8f0d742b0ff687cfbda441820d0
OrionDooms <orion45.dd@gmail.com>
2023-12-02 10:53:22 -0800

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if 0 < len(my_list):
+        x = my_list[0]
+        for i in my_list:
+            if i > x:
+                x = i
+        return (x)
+    else:
+        return ('None')
