1069597c1e413ff255ddbca60699792acba10c07
OrionDooms <orion45.dd@gmail.com>
2023-12-02 16:40:01 -0800

    checks if a singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head of a linked list.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ */
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL)
+		return (0);
+	else
+		return (1);
+}
