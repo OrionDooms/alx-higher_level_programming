b8a57a00fc34f0884c07fc297a02d004de7636e8
OrionDooms <orion45.dd@gmail.com>
2023-12-02 18:35:09 -0800

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,8 +7,43 @@
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *fast, *slow, *A_Half, *B_Half;
+
 	if (*head == NULL)
 		return (0);
-	else
-		return (1);
+	fast = *head;
+	slow = *head;
+	A_Half = *head;
+	while (fast->next != NULL && fast->next->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	B_Half = rev_list(slow->next);
+	while (B_Half != NULL)
+	{
+		if (A_Half->n != B_Half->n)
+			return (0);
+		A_Half = A_Half->next;
+		B_Half = B_Half->next;
+	}
+	return (1);
+}
+/**
+ * rev_list - reverse the linked list.
+ * @head: head of a linked list.
+ * Return: the listed list in reverse.
+ */
+listint_t *rev_list(listint_t *head)
+{
+	listint_t *prev = NULL, *nxt = NULL, *current = head;
+
+	while (current != NULL)
+	{
+		nxt = current->next;
+		current->next = prev;
+		prev = current;
+		current = nxt;
+	}
+	return (prev);
 }
