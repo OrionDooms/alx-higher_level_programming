6c5eab38c1e3971dbac4d2140ba681cd2be6dd40
OrionDooms <orion45.dd@gmail.com>
2023-12-03 08:59:27 -0800

    element at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return ('none')
+        return ('None')
     if idx > len(my_list):
-        return ('none')
+        return ('None')
     for i in range(len(my_list)):
         if idx == i:
             return (my_list[idx])
