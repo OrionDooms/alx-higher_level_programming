c3089faf71e956a7591b703a1e772c73a20859cc
OrionDooms <orion45.dd@gmail.com>
2023-12-06 12:07:02 -0800

    Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    i = set(set_1)
-    a = set(set_2)
-    c = i & a
-    return (c)
+    a = set(set_1)
+    b = set(set_2)
+    return (a ^ b)
