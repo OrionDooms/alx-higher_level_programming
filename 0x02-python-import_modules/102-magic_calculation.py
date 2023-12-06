b327101456bb2c0e0478ac06700327b1b7981a4f
OrionDooms <orion45.dd@gmail.com>
2023-12-05 23:51:35 -0800

    Squared simple

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import dis
+def magic_calculation(a, b):
+
+    ('add', 'sub')
+    magic_calculation_102[add, add, sub, sub]
+    if (a < b):
+       return (c)
+
+dis.dis(magic_calculation)
---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    for i in range(len(matrix)):
+        return ("{}".format(matrix[i]))
