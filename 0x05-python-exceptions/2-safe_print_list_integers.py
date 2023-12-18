dc4e8f0214e1a3425ae9823a94f965c622856904
OrionDooms <orion45.dd@gmail.com>
2023-12-18 09:45:30 -0800

    Integers division with debug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             i += 1
         except IndexError:
-            i -= 2 
+            i -= 2
             break
         except:
             print("", end="")
