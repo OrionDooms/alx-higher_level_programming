59592d1381cbc45077811c4631c5d503593933a7
OrionDooms <orion45.dd@gmail.com>
2023-12-18 08:47:58 -0800

    Print and count integers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print(my_list[i], end="")
             i += 1
         except IndexError:
             break
