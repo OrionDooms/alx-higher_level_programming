88567df674166cb5fdf19c38a30dd0167600c1b3
OrionDooms <orion45.dd@gmail.com>
2023-12-19 12:06:55 -0800

    square class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-""" A Square class"""
+"""A Square class"""
 
 
 class Square:
+    """ a classes built in __init__() function"""
     def __init__(self, size):
+        """Access class Attributes using objects
+        use . notation to access the attributes"""
         self.__size = size
