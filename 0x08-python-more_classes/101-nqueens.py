f5733779764ff8ed43a000d9a1d960b3855222d6
OrionDooms <orion45.dd@gmail.com>
2024-01-04 15:10:09 -0800

    101

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+def nqueens(N):
+    num = int(N)
+    """if type(num) != int:
+        print("N must be a number\n")
+        sys.exit(1)"""
+    if num < 4:
+        print("N must be at least 4\n")
+        sys.exit(1)
+    else:
+        result = []
+        matrix = [[i * num + j + 1 for j in range(num)] for i in range(num)]
+
+        for num in matrix:
+            print(num)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    argument = sys.argv[1]
+    nqueens(argument)
