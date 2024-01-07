8d43bb7b5fdc89cefff4e704a6a60cf04619a99d
OrionDooms <orion45.dd@gmail.com>
2024-01-07 07:53:12 -0800

    Say my name

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,8 @@
 
 def add_integer(a, b=98):
     """Function that adds two integers
-        If a or b are not integers or float, the function raise a Error massage.
+        If a or b are not integers or float,
+        the function raise a Error massage.
     """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""Say_my_name."""
+
 
 def say_my_name(first_name, last_name=""):
+    """A function that prints first name and last name
+
+    Args:
+        first_name (str): takes in a first name
+        last_name (str):  takes in a last name
+    Raises:
+        TypeError: first name or last name must be a string
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
         raise TypeError("last_name must be a string")
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
