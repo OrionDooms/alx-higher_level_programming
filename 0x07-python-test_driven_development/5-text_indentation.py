2f67c46bee8974bc3e74599a2f7118ed3c429506
OrionDooms <orion45.dd@gmail.com>
2024-01-07 10:23:01 -0800

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""Text indentation"""
+
 
 def text_indentation(text):
+    """function that prints a text with 2 new lines after
+    each of these characters: ., ? and :.
+    Args:
+        text (str): takes in text.
+    Raises:
+        TypeError: text must be a string.
+    """
     if type(text) != str:
         raise TypeError("text must be a string")
-    for i in text:
-        print(i, end="")
-        if i == "." or i == "?" or i == ":":
-            print("\n\n", end="")
+    characters = [".", "?", ":"]
+    new_text = text
+    for c in characters:
+        new_text = new_text.replace(f'{c} ', f'{c}\n\n')
+    print(new_text)
