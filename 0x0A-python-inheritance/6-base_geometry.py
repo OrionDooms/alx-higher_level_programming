9d4c6865fece3ace8d7710e6820f90ebd51746dd
OrionDooms <orion45.dd@gmail.com>
2024-01-08 10:57:59 -0800

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A function that raises an Exception with the message"""
+    def area(self):
+        raise Exception("area() is not implemented")
