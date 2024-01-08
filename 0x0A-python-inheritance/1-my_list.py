2602449f7f462c4affb25d1f3eeb3eb4c5d1b414
OrionDooms <orion45.dd@gmail.com>
2024-01-08 07:12:44 -0800

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""MyList class that inherits a list"""
+
+
+class MyList(list):
+
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        """prints a sorted list"""
+        sort = sorted(self)
+        print(sort)
