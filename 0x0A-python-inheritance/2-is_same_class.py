dc51badb7c683d6477616577e1a697345e1ae794
OrionDooms <orion45.dd@gmail.com>
2024-01-08 08:21:38 -0800

    same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A function that returns True if the object is an
+instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """If the object is an instance of the specified class
+    returns True otherwise False."""
+    if type(obj) != a_class:
+        return False
+    else:
+        return True
