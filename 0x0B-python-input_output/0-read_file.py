50186f06283f914624adde53e76feaa6aff409f7
OrionDooms <orion45.dd@gmail.com>
2024-01-09 04:42:00 -0800

    read text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """The function that reads and prints the text file"""
     with open(filename, encoding='utf-8') as f:
-        read_data = f.read()
+        for line in f:
+            print(line, end="")
     f.close()
-    print(read_data)
