82b803e6cda6187c3e2f83d5e3d136fe539eefa0
OrionDooms <orion45.dd@gmail.com>
2024-01-09 10:24:05 -0800

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""Define def to_json_string(my_obj):"""
+
+
+def to_json_string(my_obj):
+    """The function returns JSON of an object"""
+    return json.dumps(my_obj)
