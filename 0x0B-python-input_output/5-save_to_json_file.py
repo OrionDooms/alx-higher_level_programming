ef641ae16b4bac42fefc3d46e77a79bfd28a3aac
OrionDooms <orion45.dd@gmail.com>
2024-01-09 12:29:39 -0800

    save_to_json_file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""define save_to_json_file(my_obj, filename)"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
+    """The function that writes an Object to a text file,
+    using a JSON representation.
+    Args:
+        my_obj: JavaScript Object.
+        filename: text file.
+        """
     with open(filename, mode="w", encoding='utf-8') as f:
-        return f.write(json.dumps(my_obj, sort_keys=True))
+        return f.write(json.dumps(my_obj))
