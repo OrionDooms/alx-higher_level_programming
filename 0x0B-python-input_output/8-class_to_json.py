bf4f6c8391bb93270679e106074e30f3cd199ea2
OrionDooms <orion45.dd@gmail.com>
2024-01-10 08:53:43 -0800

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """define class_to_json(obj)"""
+import json
 
 
 def class_to_json(obj):
     """function that returns the dictionary description
     with data structure"""
-    return obj
+    return obj.__dict__
