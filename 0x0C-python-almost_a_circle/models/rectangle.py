8d9bf8297b2788e7f3a82f1c3b9cc08b5da6e632
OrionDooms <orion45.dd@gmail.com>
2024-01-15 08:36:00 -0800

    __str__ method that returns a message

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,8 +69,15 @@ class Rectangle(Base):
         return (self.__width * self.__height)
 
     def display(self):
-        """Display function prints a stdout Rectangle 
+        """Display function prints a stdout Rectangle
         with the character #"""
         for i in range(self.__height):
             print("#" * self.__width, end="")
             print()
+
+    def __str__(self):
+        """__str__ method returns [Rectangle] (id) x/y - width/height"""
+        return ("[Rectangle] ({}) {}/{} - {}/{}"
+                .format(self.id, self.__x,
+                    self.__y, self.__width,
+                    self.__height))
