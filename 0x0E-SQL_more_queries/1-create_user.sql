84e9d98c91ac612c28dd68f96530c48c9fcff88e
OrionDooms <orion45.dd@gmail.com>
2024-02-14 13:35:44 -0800

    A script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Creates the MySQL server user_0d_1 only If the user does not already exists.
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` WITH GRANT OPTION;
