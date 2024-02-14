81c3523b1ce9258e23cb463a94a3931d243135e5
OrionDooms <orion45.dd@gmail.com>
2024-02-14 07:13:57 -0800

    creates the table force_name on the MySQL serve

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 --creates the table force_name on the MySQL serve.
 CREATE TABLE IF NOT EXISTS `force_name`(
-	`id` INT NOT NULL, `name` VARCHAR(256) NOT NULL, PRIMARY KEY (id));
+	`id` INT , `name` VARCHAR(256) NOT NULL);
