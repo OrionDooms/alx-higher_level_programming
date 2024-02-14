0ab82865909ee7b5e699ea4c4181df011b3a3ee7
OrionDooms <orion45.dd@gmail.com>
2024-02-14 14:04:07 -0800

    A script that creates the table and description of the table

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,4 @@
---creates the table force_name on the MySQL serve.
-CREATE TABLE IF NOT EXISTS `unique_id`(`id` INT DEFAULT 1 UNIQUE,
+-- Creates a table on the MySQL server and gives unique_id description.
+CREATE TABLE IF NOT EXISTS unique_id(
+	`id` INT DEFAULT 1 UNIQUE,
 	`name` VARCHAR(256));
