149eca57a33b7f707f9de9df46e224867dda32a4
OrionDooms <orion45.dd@gmail.com>
2024-02-14 13:46:46 -0800

    A script that creates the table on your MySQL server

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,4 @@
---creates the table force_name on the MySQL serve.
-CREATE TABLE IF NOT EXISTS `force_name`(
-	`id` INT , `name` VARCHAR(256) NOT NULL);
+-- Creates the table force_name on the MySQL serve.
+CREATE TABLE IF NOT EXISTS force_name(
+	`id` INT ,
+	`name` VARCHAR(256) NOT NULL);
