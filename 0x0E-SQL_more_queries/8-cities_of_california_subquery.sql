b56736404da1805ce0e24cb0e52bfdbfc77dfda7
OrionDooms <orion45.dd@gmail.com>
2024-02-15 02:48:15 -0800

    Fix Error Unknown database by first create a database then use the database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- Liists all the cities of California in the database hbtn_0d_usa.
--- The list is sorted in ascending order by cities.id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
+-- The list is sorted in ascending order by cities.id
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
