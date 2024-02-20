38fde575798dff4216efd056910d63b781c1cb76
OrionDooms <orion45.dd@gmail.com>
2024-02-20 13:08:01 -0800

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
--- Converts database to UTF8
+-- Converts database to UTF8.
+-- Convert table and the fields.
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Convert table and the fields
 ALTER TABLE hbtn_0c_0.first_table MODIFY id INT,
 MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
