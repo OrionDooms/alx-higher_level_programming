8f7326734518b4e1555ee1cfbc9d9be94f4ecd6a
OrionDooms <orion45.dd@gmail.com>
2024-02-20 04:21:17 -0800

    My comment on task 5 cause an error

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
---A script that prints the full description of the table first_table
-SHOW TABLE first_table;
+-- A script that prints the full description of the table first_table.
+USE hbtn_0c_0;
+SHOW CREATE TABLE first_table;
