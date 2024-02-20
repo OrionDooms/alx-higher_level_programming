c82118e26bff6468db05c0efbc524a94d4e643c4
OrionDooms <orion45.dd@gmail.com>
2024-02-20 12:41:29 -0800

    lists the number of records and count the score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,4 @@
 -- Computes the average score of all records in the table.
 USE hbtn_0c_0;
-SELECT score, 'number'
-FROM second_table
-GROUP BY score
-ORDER BY 'number' DESC;
+SELECT score, COUNT(*) AS 'number' FROM second_table GROUP BY score
+ORDER BY 'number' DESC, score;
