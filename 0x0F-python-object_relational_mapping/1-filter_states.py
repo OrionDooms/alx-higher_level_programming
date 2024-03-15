f9e7d39a58fba239f702f63f0c67a4588e85214a
OrionDooms <Orion45.dd@gmail.com>
2024-03-15 11:24:28 -0700

    add a main function

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,20 +5,22 @@ from the database
 """
 import MySQLdb
 import sys
-LH = "localhost"
-PT = 3306
-UN = sys.argv[1]
-PW = sys.argv[2]
-BASE = sys.argv[3]
-db = MySQLdb.connect(host=LH, port=PT, user=UN, passwd=PW, db=BASE)
-cur = db.cursor()
-"""
-Execute select from states, find all names that started with N and
-sorted id in ascending order
-"""
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+
+if __name__ == "__main__":
+    LH = "localhost"
+    PT = 3306
+    UN = sys.argv[1]
+    PW = sys.argv[2]
+    BASE = sys.argv[3]
+    db = MySQLdb.connect(host=LH, port=PT, user=UN, passwd=PW, db=BASE)
+    cur = db.cursor()
+    """
+    Execute select from states, find all names that started with N and
+    sorted id in ascending order
+    """
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
