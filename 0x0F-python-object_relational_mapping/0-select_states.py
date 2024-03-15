aa9a9754495b3c993dd972d2484bbaa51bb43ff2
OrionDooms <Orion45.dd@gmail.com>
2024-03-15 11:35:06 -0700

    add a main function

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,19 @@
 """ This script lists all states from the database"""
 import MySQLdb
 import sys
-LH = "localhost"
-PT = 3306
-U = sys.argv[1]
-PW = sys.argv[2]
-BASE = sys.argv[3]
-db = MySQLdb.connect(host=LH, port=PT, user=U, passwd=PW, db=BASE)
-cur = db.cursor()
-"""Execute select from states and sorted id in ascending order"""
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+
+if __name__ == "__main__":
+    LH = "localhost"
+    PT = 3306
+    U = sys.argv[1]
+    PW = sys.argv[2]
+    BASE = sys.argv[3]
+    db = MySQLdb.connect(host=LH, port=PT, user=U, passwd=PW, db=BASE)
+    cur = db.cursor()
+    """Execute select from states and sorted id in ascending order"""
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
