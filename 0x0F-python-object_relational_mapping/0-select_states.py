2d162435182865a6b8e9a68b4867b7413d320380
OrionDooms <Orion45.dd@gmail.com>
2024-03-15 06:18:04 -0700

    a script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ This script lists all states from the database"""
 import MySQLdb
+import sys
 LH = "localhost"
 PT = 3306
-U = "root"
-PW = "root"
-BASE = "hbtn_0e_0_usa"
+U = sys.argv[1]
+PW = sys.argv[2]
+BASE = sys.argv[3]
 db = MySQLdb.connect(host=LH, port=PT, user=U, passwd=PW, db=BASE)
 cur = db.cursor()
 """Execute select from states and sorted id in ascending order"""
