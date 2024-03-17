15b0326f3bb3788cfaab522d1048dbeaf1eb86c3
OrionDooms <Orion45.dd@gmail.com>
2024-03-17 13:41:36 -0700

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,15 +2,22 @@
 """
 A python file that contains the class definition of a State
 """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+import sys
+if __name__ == "__main__":
+    UN = sys.argv[1]
+    PW = sys.argv[2]
+    BASE = sys.argv[3]
+    DB = ("mysql+mysqldb://{}:{}@localhost:3306/{}".format(UN, PW, BASE))
+    engine = create_engine(DB)
+    session = sessionmaker(bind=engine)
+    session = session()
+    states = session.query(State).all()
 
+    for S in states:
+        print(S.id, S.name, sep=": ")
 
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, unique=True, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
-~                                               
+    session.close()
