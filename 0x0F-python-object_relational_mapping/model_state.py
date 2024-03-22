1a841e08e8a4d07978532d6639ffd8c6ac4f34bb
OrionDooms <Orion45.dd@gmail.com>
2024-03-22 16:31:52 -0700

    update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,7 +6,7 @@ A python file that contains the class definition of a State
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 mymetadata = MetaData()
-Base = declarative_base()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
