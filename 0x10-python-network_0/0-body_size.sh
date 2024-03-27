3c11f73e5fbcb1fd26d5133f9d8a9bb2362675e8
OrionDooms <Orion45.dd@gmail.com>
2024-03-27 12:09:59 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#A bash script  that takes in a URL, sends a request to that URL
+#A bash script that takes in a URL, sends a request to that URL
 #and displays the size of the body.
-URL="$1"
-curl -s $URL | wc -c
+curl -s "$1" | wc -c
