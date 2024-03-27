860fb2992e250d0b6d6c717e2e0dc1fbfdb23ffb
OrionDooms <Orion45.dd@gmail.com>
2024-03-27 13:35:41 -0700

    cURL Method

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A Bash script that takes in a URL, sends a GET request and displays the body
-curl -s "$1"
+curl -s -L $1
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#A Bash script that that sends a DELETE request to the URL.
+curl -X DELETE -s $1
