3995e0863de5ad13e1a82de57cce63c57580d9cc
OrionDooms <Orion45.dd@gmail.com>
2024-03-27 14:23:43 -0700

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A Bash script takes in a URL and displays all HTTP methods the server
-curl -siX OPTIONS $1 | grep -i "allow:" | cut -d ':' -f 2
+curl -s -i -X OPTIONS $1 | grep -i "allow:" | cut -d ':' -f 2
