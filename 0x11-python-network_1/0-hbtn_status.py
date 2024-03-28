31a8ce985e9027c307a7fc2c4f8e1d46d240ec8d
OrionDooms <Orion45.dd@gmail.com>
2024-03-28 07:47:23 -0700

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""A Python script that fetches well using urllib to display the
+content of a website
+"""
+def fetch_status():
+    import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
 
+if __name__ == "__main__":
+    fetch_status()
