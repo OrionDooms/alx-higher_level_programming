9a0d9c289c657b481e583108dcde5c9442531177
OrionDooms <Orion45.dd@gmail.com>
2024-03-28 13:44:18 -0700

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""
+This script takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id
+"""
 
+import urllib.request
+import sys
+
+
+def html_request(url):
+
+    with urllib.request.urlopen(url) as response:
+        request = response.headers.get('X-Request-Id')
+        print(request)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    html_request(url)
