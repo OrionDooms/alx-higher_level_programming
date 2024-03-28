e62c9c3f68a9f3610279a77cef49d2b265702d30
OrionDooms <Orion45.dd@gmail.com>
2024-03-28 14:38:05 -0700

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id
+This script takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body.
 """
 
 import urllib.request
